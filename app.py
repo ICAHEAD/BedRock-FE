6dcd99f986c7277909cca0a12932b5de3849e90f
ICAHEAD <ian.cox@ahead.com>
2023-09-12 11:40:35 -0400

    api fix and img add

---
+++ app.py
@@ -0,0 +1,53 @@
+import streamlit as st
+import pandas as pd
+import requests, json
+from keys import url, bedrock_apikey
+
+# Define the Streamlit app
+def main():
+    st.set_page_config(
+        page_title="Flinstones Portal",
+        page_icon="dino.jpg",  # page icon logo
+        layout="wide",
+    )
+    # Display the logo at the top
+    st.image("header.png", use_column_width=True)
+
+    # Add a title and description
+    st.title("Flinstones Portal")
+    st.write("Welcome to the Flinstones 'RAG' Portal. This page allows you to interact with AHEAD's document corpus AND Bedrock LLMs!")
+
+    # Create an input box for text
+    user_input = st.text_input("Enter your LLM query here:")
+
+
+    if user_input:
+        query = user_input
+
+        # Define the JSON data you want to send
+        data = {
+            "body": query
+        }
+
+        # Set the headers to indicate that you are sending JSON data
+        headers = {
+            'Content-Type': 'application/json',
+            'x-api-key': bedrock_apikey
+        }
+
+        # Send the POST request
+        response = requests.post(url, data=json.dumps(data), headers=headers)
+
+        # Handle the response
+        if response.status_code == 200:
+            st.write("Request was successful!")
+            st.write("Response:", response.text)
+            #st.json(response.json())  # Display response JSON data
+        else:
+            st.write("Request failed with status code:", response.status_code)
+            st.write("Response:", response.text)
+            #st.json(response.json())  # Display response JSON data
+
+
+if __name__ == "__main__":
+    main()
